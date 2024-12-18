89d8693d453be04a41f7a3282ecce056a429e9a2
alisonsavi64 <alisonsavi64@gmail.com>
2024-12-17 22:29:08 -0300

    Laravel, tailwind, and vue with inertia setup

---
+++ web/resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <Layout>
+         <h1 class="text-red-500">Home</h1>
+    </Layout>
+
+
+</template>
+
+<script>
+    import Layout from '../Shared/Layout.vue';
+    export default {
+        components: {
+            Layout
+        }
+    }
+</script>
---
+++ web/resources/js/Shared/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <nav class="h-screen bg-black text-white items-center flex flex-col w-40 space-between">
+        <div>
+            <img src="../../../public/images/logo.png" alt="Logo" />
+        </div>
+        <div>
+            <ul>
+                <li>Minhas Finanças</li>
+                <li>Teste1</li>
+                <li>Teste2</li>
+            </ul>
+        </div>
+    </nav>
+
+</template>
+
+<script>
+    export default {}
+</script>
